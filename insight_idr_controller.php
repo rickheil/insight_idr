c92b612d60f527ea32a19193714c06c0a1bfea50
Rick Heil <rheil@mergeworld.com>
2019-08-23 14:41:38 -0400

    add version function to feed widget

--- insight_idr_controller.php
+++ insight_idr_controller.php
@@ -64,4 +64,34 @@ class Insight_idr_controller extends Module_controller
         $agent_status_stats = new Insight_idr_model;
         $obj->view('json', array('msg' => $agent_status_stats->get_agent_status_stats()));
     }
+
+    /**
+     * Get stats on agent versions
+     *
+     * @author rickheil
+     **/
+    public function get_agent_version_stats()
+    {
+        if(! $this->authorized()) {
+            $obj->view('json', array('msg' => array('error' => 'Not authenticated')));
+            return;
+        }
+        
+        $agent_version_stats = new Insight_idr_model();
+        $sql = "SELECT count(1) as count, agent_version
+                FROM insight_idr
+                LEFT JOIN reportdata USING (serial_number)
+                ".get_machine_group_filter()."
+                GROUP BY agent_version
+                ORDER BY agent_version DESC";
+
+        $out = [];
+        foreach ($agent_version_stats->query($sql) as $obj) {
+            $obj->agent_version = $obj->agent_version ? $obj->agent_version : '0';
+            $out[] = array('label' => $obj->agent_version, 'count' => intval($obj->count));
+        }
+
+        $obj = new View();
+        $obj->view('json', array('msg' => $out));
+    }
 }
