3dcaa3c1779740881028e2b2747b7dcf41697357
Rick Heil <rheil@mergeworld.com>
2019-08-23 14:46:02 -0400

    make provides match reality re: widget names

--- provides.php
+++ provides.php
@@ -5,8 +5,8 @@ return array(
         'insight_idr' => array('view' => 'insight_idr_listing', 'i18n' => 'insight_idr.title'),
     ),
     'widgets' => array(
-        'idr_agent_version' => array('view' => 'idr_agent_version_widget'),
-        'idr_agent_status' => array('view' => 'idr_agent_status_widget'),
+        'idr_agent_version' => array('view' => 'insight_idr_agent_version_widget'),
+        'idr_agent_status' => array('view' => 'insight_idr_agent_status_widget'),
     ),
     'reports' => array(
         'insight_idr' => array('view' => 'insight_idr_report', 'i18n' => 'insight_idr.insight_idr_report')
