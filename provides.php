3e3f43ae107c11ed101cc74e26c68b762bccfd24
Rick Heil <rheil@mergeworld.com>
2019-08-23 10:44:49 -0400

    first draft of provides.php

---
+++ provides.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'listings' => array(
+        'insight_idr' => array('view' => 'insight_idr_listing', 'i18n' => 'insight_idr.title'),
+    ),
+    'widgets' => array(
+        'idr_agent_version' => array('view' => 'idr_agent_version_widget'),
+        'idr_agent_status' => array('view' => 'idr_agent_status_widget'),
+    ),
+    'reports' => array(
+        'insight_idr' => array('view' => 'insight_idr_report', 'i18n' => 'insight_idr.insight_idr_report')
+    ),
+);
